cb72abcd8bac1425de3801733b25ef53d874ada3
Churchillok <churchillok@gmail.com>
2026-01-01 05:48:55 -0600

    Fix folder structure

--- src/components/common/auth/index.ts
+++
@@ -1,7 +0,0 @@
-// ============================================
-// AUTH - Export all auth screens
-// ============================================
-
-export { SplashScreen } from './SplashScreen';
-export { SignupScreen } from './SignupScreen';
-export { LoginScreen } from './LoginScreen';
